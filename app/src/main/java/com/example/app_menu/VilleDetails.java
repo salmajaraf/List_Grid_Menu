50af6ba580d274d65cc0c17669b601ecbcb4866c
YE <youssef.ejjiyar-etu@etu.univh2c.ma>
2024-03-01 20:22:40 +0100

    ville det

--- app/src/main/java/com/example/app_menu/VilleDetails.java
+++ app/src/main/java/com/example/app_menu/VilleDetails.java
@@ -2,13 +2,25 @@ package com.example.app_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VilleDetails extends AppCompatActivity {
-
+    Button b80;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ville_details);
+
+        b80=findViewById(R.id.b80);
+        b80.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1= new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i1);
+            }
+        });
     }
 }
