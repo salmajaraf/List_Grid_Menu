7995bb984faae56f6a938285f6947ff1a42cd4e5
Jaraf Salma <115297113+salmajaraf@users.noreply.github.com>
2024-03-01 19:59:21 +0100

    last mod

--- app/src/main/java/com/example/app_menu/MainActivity.java
+++ app/src/main/java/com/example/app_menu/MainActivity.java
@@ -2,15 +2,40 @@ package com.example.app_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button amis,pays,fruits;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        amis=findViewById(R.id.Amis);
+        pays = findViewById(R.id.Pays);
+        fruits = findViewById(R.id.Fruits);
+        amis.setOnClickListener(this);
+        pays.setOnClickListener(this);
+        fruits.setOnClickListener(this);
+    }
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        Class destinationActivity = null;
+        if (v.getId() == R.id.Fruits) {
+            destinationActivity = Fruits.class;
+        } else if (v.getId() == R.id.Amis) {
+            destinationActivity = Amis.class;
+        } else if (v.getId() == R.id.Pays) {
+            destinationActivity = Pays.class;
+        }
+        Intent intent = new Intent(MainActivity.this, destinationActivity);
+        startActivity(intent);
     }
 
     @Override
